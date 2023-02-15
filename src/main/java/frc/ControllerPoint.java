ea1fc5a3b8f20977163ac68fd28c2889a94b62aa
Bob Hedrick <vettenoir@gmail.com>
2023-02-14 20:17:18 -0500

    Implemented graph math

---
+++ src/main/java/frc/ControllerPoint.java
@@ -0,0 +1,12 @@
+package frc;
+
+public class ControllerPoint {
+    public double m_angle = 0.0;  // angle in deg. (x axis of graph)
+    public double m_power = 0.0;  // power to motor (0-1) (y axis of graph)
+
+    public ControllerPoint(double angle, double power)
+    {
+        m_angle = angle;
+        m_power = power;
+    }
+}
